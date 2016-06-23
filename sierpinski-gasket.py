590bb0abe6622d819d3e498b80c5f08734e9c0e1
Will Schumacher <meintodengel@gmail.com>
2016-06-23 14:48:48 -0700

    incomplete solution

---
+++ sierpinski-gasket.py
@@ -0,0 +1,12 @@
+def sierpinski(n):
+    if n == 0: return 'L'
+
+    spaces = [' ', ' ']
+    first = ['L', ' ']
+    second = ['L', 'L']
+
+    for i in xrange(1, n+1):
+        for j in xrange(1, n+1):
+            
+
+    return result
